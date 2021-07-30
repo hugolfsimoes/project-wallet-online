0dca9908a628bdf46151d45165d8c31eef2b6be1
Hugo Simoes <hugolfsimoes@gmail.com>
2021-07-30 11:15:56 -0300

    criando arquivo do header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <img src="" alt="" />
+      <p>Screen Name</p>
+      <img src="" alt="" />
+    </header>
+  )
+}
--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 export default function Calculator() {
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
