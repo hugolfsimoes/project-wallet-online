e15eb4563dfc42886f34c84c4c151d9a9afbc3f6
Hugo Simoes <hugolfsimoes@gmail.com>
2021-08-02 21:38:18 -0300

    implementando verificacao de email e senha no login

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -5,12 +5,28 @@ import MyContext from '../providers/MyContext';
 export default function FormLogin() {
   const history = useHistory();
   const {user, setUser} = React.useContext(MyContext)
-  const [inputUser, setInputUser] = useState();
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+  const [isDisable, setIsDisabled] = useState(true);
+
+  const emailValidationRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+  const minimalPasswordLength = 6;
+
+  useEffect(() => {
+    // Verificação de email adquirido através do site do Stackoverflow no link https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+    if (emailValidationRegex.test(inputEmail) && inputPassword.length >= minimalPasswordLength) {
+      setIsDisabled(false)
+    }else{
+      setIsDisabled(true)
+    }
+  },[inputEmail, inputPassword])
 
   function handleClickUser() {
-    setUser(inputUser);
-    localStorage.setItem('user', JSON.stringify(inputUser));
-    console.log(user);
+    setUser({
+      email: inputEmail,
+      password: inputPassword
+    });
+    localStorage.setItem('user', JSON.stringify(inputEmail));
     history.push('/profile')
   }
   
@@ -18,14 +34,14 @@ export default function FormLogin() {
   return (
     <form>
       <label htmlFor="email-input">E-mail
-        <input id="email-input" type="text" onChange={(e) => setInputUser(e.target.value)}/>
+        <input id="email-input" type="text" onChange={(e) => setInputEmail(e.target.value)}/>
       </label>
 
       <label htmlFor="password-input">Password
-        <input id="password-input" type="text"/>
+        <input id="password-input" type="text" onChange={(e) => setInputPassword( e.target.value)}/>
       </label>
 
-      <button type="button" onClick={ handleClickUser }>Login</button>
+      <button type="button" onClick={ handleClickUser } disabled={isDisable}>Login</button>
       <Link to='/aaa'>Or sign up</Link>
     </form>
   )
--- src/providers/DataProvider.js
+++ src/providers/DataProvider.js
@@ -3,7 +3,10 @@ import React, { useState } from 'react';
 import MyContext from './MyContext';
 
 export default function DataProvider({children}) {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
   
   return (
     <MyContext.Provider value={{user, setUser}} >
